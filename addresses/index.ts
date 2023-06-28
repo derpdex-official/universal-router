4ed28ec84e6899c4b65310762e0c00ff1f5b97e6
rustymuggledev <rustymuggledev@proton.me>
2023-06-28 11:43:14 +0530

    add address file

---
+++ addresses/index.ts
@@ -0,0 +1,20 @@
+export default {
+    local: {
+        WETH: "",
+        PERMIT2: "",
+        V3_FACTORY: "",
+        UNSUPPORTED: ""
+    },
+    testnet: {
+        WETH: "0xC3ec043C150c945652A09D7E47F856AC9fB0F893",
+        PERMIT2: "0x7dBaee32F615b50B318A8650eE23214768d25b34",
+        V3_FACTORY: "0x9C260E394a96BB3E6836dAE8B9f2075D0b128e83",
+        UNSUPPORTED: ""
+    },
+    mainnet: {
+        WETH: "0x5aea5775959fbc2557cc8789bc1bf90a239d9a91",
+        PERMIT2: "",
+        V3_FACTORY: "",
+        UNSUPPORTED: ""
+    }
+}
